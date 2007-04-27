02d83fce2a852a9a9eeb351d7833a936cc184865
mfoerg <mfoerg@4c04a858-cbc6-fecd-9a3c-8869a20a5432>
2007-04-27 20:22:01 +0000

    forgot to include config.h

--- trunk/src/wrapper.cc
+++ trunk/src/wrapper.cc
@@ -19,6 +19,8 @@
  *
  */
 
+#include "config.h"
+
 extern "C" {
 #include "cblas.h"
   void dgeev_( char* jobvl, char* jobvr, int *n, double *a, int *lda,double * wr,double * wi,double * vl, int *ldvl,double* vr,int * ldvr,double* work,int * lwork, int *info );
