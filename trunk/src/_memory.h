41e10355b2958164db1142b82e9074d80f5b5187
mafriedrich <mafriedrich@4c04a858-cbc6-fecd-9a3c-8869a20a5432>
2011-08-11 19:31:38 +0000

    Changes in source code: - Removed MemoryStack allocator: unstable; some of the performance tests below crashed - Added new allocator PoolAllocator: thread safe if required; see preformance tests below
    Changes in build configuration:
    - Removed configure option --disable-pragma_omp_critical
    - Added configure option --enable-threadsafety[=TYPE] which enables thread safety in fmatvec
      * TYPE=pthread: used pthread functions for thread safety
      * TYPE=gccbuiltin: use gnu gcc builtin atomic operations for thread safety
      * TYPE=openmp, or TYPE omitted: use openmp for thread safety
      * TYPE=no, or option not given: disable thread safety (fastest method for non threaded applications)
    - Changed default behaviour:
      * The default allocator is now the new fmatvec::PoolAllocator
      * The default is now NO thread safety (you MUST configure with --enable-threadsafety for threaded applications)
    
    
    
    PERFORMANCE TESTS
    
    were done using different allocators and different thread safety methods using the MBSim example xml_chaintainsioner:
    
    Simulation time with --enable-threadsafety=gccbuiltin
    fmatvec::MemoryStack (20,0)         43.4sec
    __gnu_cxx::__pool_alloc:            43.2sec
    fmatvec::MemoryStack (20,10000)     39.9sec (unstable)
    fmatvec::PoolAllocator:             38.7sec
    __gnu_cxx::bitmap_allocator:        37.9sec
    __gnu_cxx::__mt_alloc:              37.3sec
    std::allocator:                     33.7sec
    __gnu_cxx::new_allocator:           33.6sec
    __gnu_cxx::malloc_allocator:        33.2sec
    
    Simulation time with --enable-threadsafety=openmp
    fmatvec::MemoryStack (20,0)         32.9sec
    std::allocator:                     30.5sec
    fmatvec::MemoryStack (20,10000)     26.5sec (unstable)
    fmatvec::PoolAllocator:             26.5sec
    
    Simulation time with --enable-threadsafety=pthread
    fmatvec::MemoryStack (20,0)         53.2sec
    fmatvec::MemoryStack (20,10000)     51.3sec (unstable)
    fmatvec::PoolAllocator:             49.5sec
    std::allocator:                     42.6sec
    
    Simulation time with --enable-threadsafety=no
    std::allocator:                     31.0sec
    fmatvec::MemoryStack (20,0)         31.0sec
    fmatvec::MemoryStack (20,10000)     26.2sec (unstable)
    fmatvec::PoolAllocator:             25.6sec
    
    SUMMARY (recommended settings):
      - use always the allocator: fmatvec::PoolAllocator
      - use --enable-threadsafety for threaded applications

--- trunk/src/_memory.h
+++ trunk/src/_memory.h
@@ -22,11 +22,11 @@
 #ifndef _memory_h
 #define _memory_h
 
+// includes
 #include <stdlib.h>
+#include <vector>
 
-#define MSTACKSIZE  20
-#define MAXSZ 0
-
+// include header given by ALLOCATORHEADER
 #ifdef ALLOCATORHEADER
 #  define CPPBEGININCLUDE <
 #  define CPPENDINCLUDE >
@@ -34,104 +34,141 @@
 #  undef CPPBEGININCLUDE
 #  undef CPPENDINCLUDE
 #endif
+// use fmatvec::PoolAllocator if no allocator is defined by ALLOCATORCLASS
 #ifndef ALLOCATORCLASS
-#  define ALLOCATORCLASS fmatvec::MemoryStack
+#  define ALLOCATORCLASS fmatvec::PoolAllocator
 #endif
 
-  /**
-   * \brief memory managment and reference counting 
-   * \author Martin Foerg
-   * \date 2010-07-07 pragma omp critical can be deactivated by --disable-pragma_omp_critical (Robert Huber)
-   */
+// create some locking and atomic operations dependent on the thread safety method
+//
+// FMATVEC_LOCKVAR(l) : create a locking/mutex named l
+// FMATVEC_LOCKINIT(l) : initialize the locking/mutex named l
+// FMATVEC_LOCK(l) : lock the locking/mutex l
+// FMATVEC_UNLOCK(l) : unlock the locking/mutex l
+//
+// FMATVEC_SYNCVAR(l) : create a locking/mutex named l for a syncronized operation
+// FMATVEC_SYNCINIT(l) : initialize a locking/mutex named l for a syncronized operation
+// FMATVEC_SYNCINC(var, l) : var++ (syncronized by l)
+// FMATVEC_SYNCPREDEC(ret, var, l) : ret=--(var) (syncronized by l)
+#if defined FMATVEC_THREADSAFE_GCCBUILTIN
+  #define FMATVEC_LOCKVAR(l) short int l;
+  #define FMATVEC_LOCKINIT(l) l=0;
+  #define FMATVEC_LOCK(l) while(__sync_bool_compare_and_swap(&l, 0, 0xffff)==false);
+  #define FMATVEC_UNLOCK(l) __sync_bool_compare_and_swap(&l, 0xffff, 0);
+
+  #define FMATVEC_SYNCVAR(l)
+  #define FMATVEC_SYNCINIT(l)
+  #define FMATVEC_SYNCINC(var, l) __sync_add_and_fetch(&(var), 1);
+  #define FMATVEC_SYNCPREDEC(ret, var, l) ret=__sync_sub_and_fetch(&(var), 1);
+#elif defined FMATVEC_THREADSAFE_PTHREAD
+  #include <pthread.h> // pthread required a header file
+
+  #define FMATVEC_LOCKVAR(l) pthread_mutex_t l;
+  #define FMATVEC_LOCKINIT(l) pthread_mutex_init(&l, NULL);
+  #define FMATVEC_LOCK(l) pthread_mutex_lock(&l);
+  #define FMATVEC_UNLOCK(l) pthread_mutex_unlock(&l);
+
+  #define FMATVEC_SYNCVAR(l)  pthread_mutex_t l;
+  #define FMATVEC_SYNCINIT(l) pthread_mutex_init(&l, NULL);
+  #define FMATVEC_SYNCINC(var, l) pthread_mutex_lock(&l); (var)++; pthread_mutex_unlock(&l);
+  #define FMATVEC_SYNCPREDEC(ret, var, l) pthread_mutex_lock(&l); ret=--(var); pthread_mutex_unlock(&l);
+#elif defined FMATVEC_THREADSAFE_OPENMP
+  #define FMATVEC_PRAGMA(x) _Pragma(#x) // openmp required the _Pragma operator (defined by C99)
+
+  #define FMATVEC_LOCKVAR(l)
+  #define FMATVEC_LOCKINIT(l)
+  #define FMATVEC_LOCK(l) FMATVEC_PRAGMA(omp critical (l)) {
+  #define FMATVEC_UNLOCK(l) }
+
+  #define FMATVEC_SYNCVAR(l)
+  #define FMATVEC_SYNCINIT(l)
+  #define FMATVEC_SYNCINC(var, l) FMATVEC_PRAGMA(omp critical (l)) { (var)++; }
+  #define FMATVEC_SYNCPREDEC(ret, var, l) FMATVEC_PRAGMA(omp critical (l)) { ret=--(var); }
+#else
+  #define FMATVEC_PRAGMA(l)
+  #define FMATVEC_LOCKVAR(l)
+  #define FMATVEC_LOCKINIT(l)
+  #define FMATVEC_LOCK(l)
+  #define FMATVEC_UNLOCK(l)
+
+  #define FMATVEC_SYNCVAR(l)
+  #define FMATVEC_SYNCINIT(l)
+  #define FMATVEC_SYNCINC(var, l) (var)++;
+  #define FMATVEC_SYNCPREDEC(ret, var, l) ret=--(var);
+#endif
+
+/**
+ * \brief memory managment and reference counting 
+ * \author Martin Foerg
+ * \date 2010-07-07 pragma omp critical can be deactivated by --disable-pragma_omp_critical (Robert Huber)
+ */
 
 namespace fmatvec {
 
  /// @cond NO_SHOW
 
-  template <class AT> class MemoryStack {
-      private:
-	AT ***ele;
-      public:
-	MemoryStack() {
-	  ele=new AT**[MAXSZ];
-	  for(int i=0; i<MAXSZ; i++)
-	    ele[i]=0;
-	}
-	~MemoryStack() {
-	  for(int i=0; i<MAXSZ; i++) {
-	    AT **&sp=ele[i];
-	    if(sp) {
-	      while(*sp) {
-		sp--;
-	      }
-	      for(int j=0; j<MSTACKSIZE; j++)
-		if(sp[j]!= (AT *) 1)
-		  delete [] ele[i][j];
-	      delete [] sp;
-	    }
-	  }
-	  delete [] ele; 
-	}
-
-	AT** renewstack(AT ** sp) {
-	  AT **sp_neu,**sp_merk=sp;
-	  int alte_laenge,neue_laenge,i;
-	  while(*(--sp)); 
-	  alte_laenge=sp_merk-sp+1;
-	  neue_laenge=alte_laenge+MSTACKSIZE;
-	  sp_neu = new AT*[neue_laenge];
-	  for(i=0;i<alte_laenge-1;i++) sp_neu[i]=sp[i];
-	  delete[] sp;
-	  for(i=alte_laenge;i<neue_laenge-1;i++) sp_neu[i] = (AT *) 1;
-	  sp_neu[neue_laenge-1]=NULL;
-	  return sp_neu+alte_laenge-1;
-	}
-
-	AT** newstack() {
-	  AT** sp;
-	  AT** stackptr;
-	  sp = stackptr = new AT*[MSTACKSIZE];
-	  for (int i=0; i<MSTACKSIZE; i++) *(sp++) = (AT *) 1;
-	  stackptr[0]=NULL; stackptr[MSTACKSIZE-1]=NULL;
-	  return stackptr;
-	}
-
-	AT * allocate(size_t sz) { 
-          AT *ret;
-#ifdef PRAGMA_OMP_CRITICAL         
-#         pragma omp critical (fmatvec_MemoryStack_deAllocate)
-#endif 
-          {
-	    AT **&sp=ele[sz];
-	    if(sz<MAXSZ) {
-	      if(!sp)
-	        sp = newstack();
-	      if (*sp) 
-	        ret=*sp--;
-	      else 
-	        ret=new AT[sz]; 
-	    } else
-	      ret=new AT[sz];  
+  // A STL-like pool memory allocator: fast but NOT thread safe till now!
+  template <class AT>
+  class PoolAllocator {
+    private:
+      // The minimal allocation size in power of 2: MINSIZE=2^MINEXP
+      static const size_t MINEXP=4;
+      // A pool of memory of allocation size 2^(MINEXP+0), 2^(MINEXP+1), 2^(MINEXP+2), ...
+      // stored at index 0, 1, 2, ... in memoryPool.
+      // The pointers stored here are already allocated but currently not used.
+      std::vector<std::vector<AT*> > memoryPool;
+      // Just a dummy variable used for zero size allocations.
+      AT sizeZero;
+      FMATVEC_LOCKVAR(fmatvec_memoryPoolLock)
+    public:
+      // Constructor: alloc memoryPool with at least size 1, since the size is doubled each time the size ran out.
+      PoolAllocator() : memoryPool(1) {
+        FMATVEC_LOCKINIT(fmatvec_memoryPoolLock)
+      }
+      // Destructor: delete all cached memory in the pool.
+      ~PoolAllocator() {
+        // loop over all pool sizes and free memory
+        for(typename std::vector<std::vector<AT*> >::iterator i=memoryPool.begin(); i!=memoryPool.end(); ++i)
+          for(typename std::vector<AT*>::iterator j=i->begin(); j!=i->end(); ++j)
+            delete[](*j);
+      }
+      // Allocate new memory of at least size size.
+      AT *allocate(size_t size) { 
+        // do nothing for zero size, but return a valid memory pointer
+        if(size==0) return &sizeZero;
+        // calculate size index: size 0 to 2^MINEXP = index 0; size 2^MINEXP+1 to 2*2^MINEXP = index 1; ...
+        size_t index=0;
+        --size>>=MINEXP-1;
+        while(size>>=1) ++index;
+        AT* ret=NULL;
+        FMATVEC_LOCK(fmatvec_memoryPoolLock)
+          // increase (double) memoryPool if nessasary
+          if(index>=memoryPool.size()) memoryPool.resize(index<<1);
+          // try to use cached memory
+          std::vector<AT*> &memoryPoolIndex=memoryPool[index];
+          if(memoryPoolIndex.size()>0) {
+            ret=memoryPoolIndex.back(); // get last cached memory
+            memoryPoolIndex.pop_back(); // remove last cached memory
           }
-          return ret;
-	}
-	void deallocate(AT *p, size_t sz) {
-#ifdef PRAGMA_OMP_CRITICAL         
-#         pragma omp critical (fmatvec_MemoryStack_deAllocate)
-#endif 
-          {
-	    AT **&sp=ele[sz];
-	    if(sz<MAXSZ && sz) {
-	      if(*(++sp)) 
-	        *sp = p; 
-	      else {
-	        sp=renewstack(sp);
-	        *sp=p;
-	      }
-	    } else delete [] p;
-          }
-	}
-    };
+        FMATVEC_UNLOCK(fmatvec_memoryPoolLock)
+        if(ret) return ret; // return chaced memory
+        // return new memory
+        return new AT[1<<(index+MINEXP)]; // allocate 2^(index+MINEXP) elements (max size for this index)
+      }
+      // Free memory and release it to the pool.
+      void deallocate(AT *p, size_t size) {
+        // do nothing for zero size, a dummy but valid pointer was returned by allocate
+        if(size==0) return;
+        // calculate size index: size 0 to 2^MINEXP = index 0; size 2^MINEXP+1 to 2*2^MINEXP = index 1; ...
+        size_t index=0;
+        --size>>=MINEXP-1;
+        while(size>>=1) ++index;
+        FMATVEC_LOCK(fmatvec_memoryPoolLock)
+          // release memory
+          memoryPool[index].push_back(p); // add to cached memory
+        FMATVEC_UNLOCK(fmatvec_memoryPoolLock)
+      }
+  };
 
   template <class AT> class Memory {
       private:
@@ -140,18 +177,13 @@ namespace fmatvec {
 	AT *ele0;
         static allocator_type ms;
 	size_t *ref;
+        FMATVEC_SYNCVAR(fmatvec_refLock)
 	void lock() {
-#ifdef PRAGMA_OMP_CRITICAL        
-#         pragma omp critical (fmatvec_Memory_ref)
-#endif
-          (*ref)++;
+          FMATVEC_SYNCINC(*ref, fmatvec_refLock)
         };
 	void unlock() {
           size_t refLocal;
-#ifdef PRAGMA_OMP_CRITICAL 
-#         pragma omp critical (fmatvec_Memory_ref)
-#endif
-          refLocal=--(*ref);
+          FMATVEC_SYNCPREDEC(refLocal, *ref, fmatvec_refLock)
 	  if(!refLocal) {
             ms.deallocate(ele0, sz);
 	    delete ref;
@@ -159,13 +191,16 @@ namespace fmatvec {
 	};
 
       public:
-	Memory() : sz(0), ele0(0), ref(new size_t(1)){ 
+	Memory() : sz(0), ele0(0), ref(new size_t(1)) {
+          FMATVEC_SYNCINIT(fmatvec_refLock);
 	};
 
 	Memory(size_t n) : sz(n), ele0(ms.allocate(sz)), ref(new size_t(1)){
+          FMATVEC_SYNCINIT(fmatvec_refLock);
 	};
 
 	Memory(const Memory &memory) : sz(memory.sz), ele0(memory.ele0), ref(memory.ref)  {
+          FMATVEC_SYNCINIT(fmatvec_refLock);
 	  lock();
 	};
 
