24f78f409ecab656eec1c78ad4db4a36b4cbded9
Robert Kühberger <robert@kuehberger.consulting>
2020-05-11 16:52:28 +0200

    Add #include linear_algebra_complex.h
    Otherwise, the following will not get exported under gcc:
    fmatvec::slvLU(fmatvec::SquareMatrix<fmatvec::Ref, std::complex<double> > const&, fmatvec::Vector<fmatvec::Ref, std::complex<double> > const&)

--- fmatvec/linear_algebra_complex.cc
+++ fmatvec/linear_algebra_complex.cc
@@ -25,7 +25,7 @@
 #include "linear_algebra_var.h"
 #include "linear_algebra_fixed_var.h"
 #include "linear_algebra_var_fixed.h"
-#include "linear_algebra_double.h"
+#include "linear_algebra_complex.h"
 #include "wrapper.h"
 #include <stdexcept>
 #include <sstream>
