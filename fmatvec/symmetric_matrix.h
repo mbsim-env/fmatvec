eba84d1324ff534504e422fbd2d5ee8c7a249a17
Martin Förg <martin.o.foerg@gmail.com>
2022-12-19 16:28:08 +0100

    Fix test for symmetry

--- fmatvec/symmetric_matrix.h
+++ fmatvec/symmetric_matrix.h
@@ -529,7 +529,7 @@ namespace fmatvec {
           throw std::runtime_error("The rows of the input have different length.");
         for(int c=r; c<cols(); c++) {
           ej(r,c)=m[r][c];
-          if(c>r && abs(m[r][c]-m[c][r])>abs(m[r][c])*1e-13+1e-13)
+          if(c>r && abs(m[r][c]-m[c][r])>abs(m[r][c]*1e-13+1e-13))
             throw std::runtime_error("The input is not symmetric.");
         }
       }
